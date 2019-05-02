1a11b0ee334edc6eae4ad629d347910bcbe02668
main <vannov@gmail.com>
2019-05-02 22:09:12 +0400

    first app

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Three/>
+  </div>
+</template>
+
+<script>
+  import Three from "./components/Three"
+
+export default {
+  name: 'app',
+  components: {
+      Three
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Three.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Three</h1>
+</template>
+
+<script>
+    export default {
+        name: "Three.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
